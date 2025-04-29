d030b27d7787a8ec5c8a9bcb39e0f72c5df61d9d
SetidetBojkov <setidet.bojkov.highschool@buditel.bg>
2025-04-30 01:10:55 +0300

    Add CategoriesController and Razor views (Add, All)

---
+++ ClothingStore/Controllers/CategoriesController.cs
@@ -0,0 +1,47 @@
+﻿using ClothingStore.Data;
+using ClothingStore.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClothingStore.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class CategoriesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /Categories
+        [AllowAnonymous]
+        public async Task<IActionResult> All()
+        {
+            var categories = await _context.Categories.ToListAsync();
+            return View(categories);
+        }
+
+        // GET: /Categories/Add
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        // POST: /Categories/Add
+        [HttpPost]
+        public async Task<IActionResult> Add(Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(category);
+            }
+
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("All");
+        }
+    }
+}
